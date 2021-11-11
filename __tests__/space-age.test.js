393995a819c5a6d5d002b888147309a135bd7acb
Matt Casperson <mc.casperson@gmail.com>
2021-11-11 09:17:19 -0800

    add third test to convert to venus age

--- __tests__/space-age.test.js
+++ __tests__/space-age.test.js
@@ -1,4 +1,3 @@
-import { TestWatcher } from "@jest/core";
 import SpaceAge from "../src/js/space-age";
 
 describe('SpaceAge', () => {
@@ -11,9 +10,12 @@ describe('SpaceAge', () => {
   test('should correctly create a SpaceAge object with the users age in earth years', () => {
     expect(myAge.earthAge).toEqual(31);
   });
-
   test('should convert earth age to mercury age (to 2 decimal places) and add it as a new property to the object', () => {
     myAge.onMercury();
     expect(myAge.mercuryAge).toEqual(129.17);
+  });
+  test('should convert earth age to venus age (to 2 decimal places) and add it as a new property to the object', () => {
+    myAge.onVenus();
+    expect(myAge.venusAge).toEqual(50);
   });
 });
