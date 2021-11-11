aea569d77f22afbbb36992dc44a0867f759f71b9
Matt Casperson <mc.casperson@gmail.com>
2021-11-11 13:41:50 -0800

    WIP add javascript for handling user input to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import SpaceAge from './js/space-age';
 
+$("#age-input").submit((event) => {
+  event.preventDefault();
+  let userAge = $("#your-age").val();
+  let yourAge = new SpaceAge(userAge);
+  yourAge.ageConverter();
+  $("#output").fadeIn();
+});
