4577e15b78778ae2116d2e05f518d81947415b22
Matt Casperson <mc.casperson@gmail.com>
2021-11-11 10:20:50 -0800

    refactor tests to adjust the beforeEach

--- __tests__/space-age.test.js
+++ __tests__/space-age.test.js
@@ -5,25 +5,22 @@ describe('SpaceAge', () => {
 
   beforeEach(() => {
     myAge = new SpaceAge(31);
+    myAge.ageConverter();
   });
 
   test('should correctly create a SpaceAge object with the users age in earth years', () => {
     expect(myAge.ages['Earth']).toEqual(31);
   });
   test('should convert earth age to mercury age (to 2 decimal places) and add it to the ages property', () => {
-    myAge.ageConverter();
     expect(myAge.ages['Mercury']).toEqual(129.17);
   });
   test('should convert earth age to venus age (to 2 decimal places) and add it to the ages property', () => {
-    myAge.ageConverter();
     expect(myAge.ages['Venus']).toEqual(50);
   });
   test('should convert earth age to mars age (to 2 decimal places) and add it to the ages property', () => {
-    myAge.ageConverter();
     expect(myAge.ages['Mars']).toEqual(16.49);
   });
   test('should convert earth age to jupiter age (to 2 decimal places) and add it to the ages property', () => {
-    myAge.ageConverter();
     expect(myAge.ages['Jupiter']).toEqual(2.61);
   });
   test('should correctly create a property with the max life expectancy on each planet', () => {
@@ -37,10 +34,12 @@ describe('SpaceAge', () => {
     expect(myAge.yearsLeft('Earth')).toEqual(47.99);
   });
   test('should calculate remaining average expectancy for chosen planet', () => {
-    myAge.ageConverter();
     expect(myAge.yearsLeft('Mercury')).toEqual(199.96);
     expect(myAge.yearsLeft('Venus')).toEqual(77.40);
     expect(myAge.yearsLeft('Mars')).toEqual(25.53);
     expect(myAge.yearsLeft('Jupiter')).toEqual(4.05);
   });
+//   test('should return foreboding message to a user not yet at the average expectancy', () => {
+    
+//   });
 });
