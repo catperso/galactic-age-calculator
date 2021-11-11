b1d1563da1ec9f8c4bdea428888501ceb69fa6d4
Matt Casperson <mc.casperson@gmail.com>
2021-11-11 09:12:54 -0800

    add method to pass second test

--- src/js/space-age.js
+++ src/js/space-age.js
@@ -2,4 +2,9 @@ export default class SpaceAge {
   constructor(age) {
     this.earthAge = age;
   }
+
+  onMercury() {
+    let mercury = (this.earthAge / 0.24).toFixed(2);
+    this.mercuryAge = parseFloat(mercury);
+  }
 }
