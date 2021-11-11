ceda51b6476476b2da7c6c3c96167b3592ec50d0
Matt Casperson <mc.casperson@gmail.com>
2021-11-11 09:29:31 -0800

    add fourth test to convert to mars age

--- __tests__/space-age.test.js
+++ __tests__/space-age.test.js
@@ -10,12 +10,16 @@ describe('SpaceAge', () => {
   test('should correctly create a SpaceAge object with the users age in earth years', () => {
     expect(myAge.ages['Earth']).toEqual(31);
   });
-  test('should convert earth age to mercury age (to 2 decimal places) and add it as a new property to the object', () => {
+  test('should convert earth age to mercury age (to 2 decimal places) and add it to the ages property', () => {
     myAge.onMercury();
     expect(myAge.ages['Mercury']).toEqual(129.17);
   });
-  test('should convert earth age to venus age (to 2 decimal places) and add it as a new property to the object', () => {
+  test('should convert earth age to venus age (to 2 decimal places) and add it to the ages property', () => {
     myAge.onVenus();
     expect(myAge.ages['Venus']).toEqual(50);
   });
+  test('should convert earth age to mars age (to 2 decimal places) and add it to the ages property', () => {
+    myAge.onMars();
+    expect(myAge.ages['Mars']).toEqual(16.49);
+  })
 });
