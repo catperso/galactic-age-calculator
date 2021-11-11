051af9072f49fd1909ecf0199341b20894f3cda0
Matt Casperson <mc.casperson@gmail.com>
2021-11-11 09:37:22 -0800

    refactor SpaceAge to have a single method for converting ages

--- __tests__/space-age.test.js
+++ __tests__/space-age.test.js
@@ -11,19 +11,19 @@ describe('SpaceAge', () => {
     expect(myAge.ages['Earth']).toEqual(31);
   });
   test('should convert earth age to mercury age (to 2 decimal places) and add it to the ages property', () => {
-    myAge.onMercury();
+    myAge.ageConverter();
     expect(myAge.ages['Mercury']).toEqual(129.17);
   });
   test('should convert earth age to venus age (to 2 decimal places) and add it to the ages property', () => {
-    myAge.onVenus();
+    myAge.ageConverter();
     expect(myAge.ages['Venus']).toEqual(50);
   });
   test('should convert earth age to mars age (to 2 decimal places) and add it to the ages property', () => {
-    myAge.onMars();
+    myAge.ageConverter();
     expect(myAge.ages['Mars']).toEqual(16.49);
   });
   test('should convert earth age to jupiter age (to 2 decimal places) and add it to the ages property', () => {
-    myAge.onJupiter();
+    myAge.ageConverter();
     expect(myAge.ages['Jupiter']).toEqual(2.61);
   });
 });
--- src/js/space-age.js
+++ src/js/space-age.js
@@ -4,22 +4,10 @@ export default class SpaceAge {
     this.ages['Earth'] = age;
   }
 
-  onMercury() {
-    let mercury = (this.ages['Earth'] / 0.24).toFixed(2);
-    this.ages['Mercury'] = parseFloat(mercury);
-  }
-
-  onVenus() {
-    let venus = (this.ages['Earth'] / 0.62).toFixed(2);
-    this.ages['Venus'] = parseFloat(venus);
-  }
-
-  onMars() {
-    let mars = (this.ages['Earth'] / 1.88).toFixed(2);
-    this.ages['Mars'] = parseFloat(mars);
-  }
-
-  onJupiter() {
+  ageConverter() {
+    this.ages['Mercury'] = parseFloat((this.ages['Earth'] / 0.24).toFixed(2));
+    this.ages['Venus'] = parseFloat((this.ages['Earth'] / 0.62).toFixed(2));
+    this.ages['Mars'] = parseFloat((this.ages['Earth'] / 1.88).toFixed(2));
     this.ages['Jupiter'] = parseFloat((this.ages['Earth'] / 11.86).toFixed(2));
   }
 }
