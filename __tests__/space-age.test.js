3ea7ab438b79cf99bebdea2b5ca4f23795878b0c
Matt Casperson <mc.casperson@gmail.com>
2021-11-11 09:24:16 -0800

    refactor SpaceAge to store ages in an object within the object

--- __tests__/space-age.test.js
+++ __tests__/space-age.test.js
@@ -8,14 +8,14 @@ describe('SpaceAge', () => {
   });
 
   test('should correctly create a SpaceAge object with the users age in earth years', () => {
-    expect(myAge.earthAge).toEqual(31);
+    expect(myAge.ages['Earth']).toEqual(31);
   });
   test('should convert earth age to mercury age (to 2 decimal places) and add it as a new property to the object', () => {
     myAge.onMercury();
-    expect(myAge.mercuryAge).toEqual(129.17);
+    expect(myAge.ages['Mercury']).toEqual(129.17);
   });
   test('should convert earth age to venus age (to 2 decimal places) and add it as a new property to the object', () => {
     myAge.onVenus();
-    expect(myAge.venusAge).toEqual(50);
+    expect(myAge.ages['Venus']).toEqual(50);
   });
 });
--- src/js/space-age.js
+++ src/js/space-age.js
@@ -1,15 +1,16 @@
 export default class SpaceAge {
   constructor(age) {
-    this.earthAge = age;
+    this.ages = {};
+    this.ages['Earth'] = age;
   }
 
   onMercury() {
-    let mercury = (this.earthAge / 0.24).toFixed(2);
-    this.mercuryAge = parseFloat(mercury);
+    let mercury = (this.ages['Earth'] / 0.24).toFixed(2);
+    this.ages['Mercury'] = parseFloat(mercury);
   }
 
   onVenus() {
-    let venus = (this.earthAge / 0.62).toFixed(2);
-    this.venusAge = parseFloat(venus);
+    let venus = (this.ages['Earth'] / 0.62).toFixed(2);
+    this.ages['Venus'] = parseFloat(venus);
   }
 }
